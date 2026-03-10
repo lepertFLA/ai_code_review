67d354cec90d4529fc88623836de10efa856e70a
lepertRSU <james@runsignup.com>
2026-03-10 18:03:01 -0400

    add infinite loop

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+			while (true) {
+				Console.WriteLine("Hello, World!");
+				Thread.Sleep(1000);
+			}
             Console.WriteLine("Hello, World!");
         }
     }
